a13bdbf0c589210b33f8560d0aadabfa1b8aba70
Shamim Shahriyar <shamimshahriyar.cse@gmail.com>
2026-06-30 10:00:39 +0600

    style: change Yes button heart emoji to white for visibility

--- src/components/ProposalFlow.jsx
+++ src/components/ProposalFlow.jsx
@@ -422,7 +422,7 @@ export default function ProposalFlow() {
                 className="btn-primary btn-yes" 
                 onClick={handleNext}
               >
-                Yes! <Icons.HeartMini />
+                Yes! <svg width="16" height="16" viewBox="0 0 24 24" fill="white" style={{ opacity: 0.9 }}><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
               </motion.button>
               <motion.button 
                 ref={noBtnRef}
